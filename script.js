316b209927e09fadfd2f62bef9a4b5b0f09b34ca
hamulu <ismailhamulu@gmail.com>
2021-04-26 11:32:13 +0300

    Yeni Eleman Oluşturuldu

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function myFunction() {
     document.querySelector("a[target]").style.border = "10px solid red";//target özelliği olan a'lara kenarlık ekle.
     document.querySelector("a[href='https://www.w3schools.com']").style.color="green";//href özelliğinin değerinie göre seçme işlemi yapıldı.
 }
+/*Yeni Element Oluşturma*/
+
+function elemanEkle(){
+    var yeniEleman=document.createElement("li");
+    yeniEleman.className="list-group-item";
+
+    //yeniEleman.innerText="A sixth item";yazı eklemenin 1.yolu
+    
+    //var icerik=document.createTextNode("A sixth item");//yazı eklemenin 2.yolu
+    //yeniEleman.appendChild(icerik);
+
+    yeniEleman.innerHTML="A sixth item<span class='badge badge-primary'>3</span>";//yazı eklemenin 3.yolu, ancak bu yöntemde html etiketleri eklemek mümkün
+
+    document.querySelector("ul.list-group").appendChild(yeniEleman);
+}
